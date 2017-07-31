cccd47d1f3b1ec602b46ef4ccf28f45644853ca8
Alkivi <admin@alkivi.fr>
2017-07-31 18:25:46 +0200

    fix

--- start.sh
+++ start.sh
@@ -8,7 +8,6 @@ echo "INFO: Starting salt-master with log level $LOG_LEVEL"
 echo "log_level: ${LOG_LEVEL}" > /etc/salt/master.d/logging.conf
 echo "log_level_logfile: quiet" >> /etc/salt/master.d/logging.conf
 
-
 # Should we start the API ?
 if [ ! -z "$SALT_API" ]; then
     echo "INFO: Starting salt-api"
